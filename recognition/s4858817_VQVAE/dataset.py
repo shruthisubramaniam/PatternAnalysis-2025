509670d2f64e73fd8d8119fda0b7e749c2cf8b8c
shruthisubramaniam <shruthisubramaniam06@gmail.com>
2025-10-09 14:46:16 +1000

    Giving names to the subfolders that already exist

--- recognition/s4858817_VQVAE/dataset.py
+++ recognition/s4858817_VQVAE/dataset.py
@@ -7,3 +7,11 @@ import numpy as np
 import torch
 from torch.utils.data import Dataset
 
+IMG_EXTS = (".nii", ".nii.gz", ".npy") # Defining how the images exist
+
+# Directory that splits names for the subfolders 
+SPLIT2SUBDIR = {
+    "train": "keras_slices_train",
+    "val":   "keras_slices_val",
+    "test":  "keras_slices_test",
+}
