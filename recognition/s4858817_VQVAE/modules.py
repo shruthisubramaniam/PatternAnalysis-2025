b1c7b241a2420d8be281f3bfbf4a04cc200becb0
shruthisubramaniam <shruthisubramaniam06@gmail.com>
2025-10-16 14:27:52 +1000

    Created the residual class that contains the residual block.

--- recognition/s4858817_VQVAE/modules.py
+++ recognition/s4858817_VQVAE/modules.py
@@ -3,3 +3,15 @@ from typing import Tuple
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
+class Residual(nn.Module):
+    def __init__(self, ch: int):
+        super().__init__()
+        self.block = nn.Sequential(
+            nn.ReLU(inplace=True),
+            nn.Conv2d(ch, ch, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(ch, ch, 1),
+        )
+    
+    def forward(self, x): return x + self.block(x)
