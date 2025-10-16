72fedeb89dd734a6d4a4b8ab9507087a17a2f144
shruthisubramaniam <shruthisubramaniam06@gmail.com>
2025-10-16 17:06:19 +1000

    fixed indenting error

--- recognition/s4858817_VQVAE/modules.py
+++ recognition/s4858817_VQVAE/modules.py
@@ -32,33 +32,33 @@ class VectorQuantizer(nn.Module):
         self.embedding = nn.Embedding(num_embeddings, embedding_dim)
         self.embedding.weight.data.uniform_(-1.0 / num_embeddings, 1.0 / num_embeddings)
 
-def forward(self, z_e: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
-    B, D, H, W = z_e.shape
-    z = z_e.permute(0, 2, 3, 1).contiguous() # (B, H, W, D)
-    flat = z.view(-1, D) # (BHW, D)
-    emb = self.embedding.weight # (K, D)
+    def forward(self, z_e: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+        B, D, H, W = z_e.shape
+        z = z_e.permute(0, 2, 3, 1).contiguous() # (B, H, W, D)
+        flat = z.view(-1, D) # (BHW, D)
+        emb = self.embedding.weight # (K, D)
 
-    # L2 sqaured distances calculated between the embeddings and the latent
-    dist = (
-        flat.pow(2).sum(dim=1, keepdim=True)
-        - 2 * flat @ emb.t()
-        + emb.pow(2).sum(dim=1)
-    ) # (BHW, K)
-    indices = torch.argmin(dist, dim=1) # (BHW, )
-    z_q = self.embedding(indices).view(B, H, W, D).permute(0, 3, 1, 2)
+        # L2 sqaured distances calculated between the embeddings and the latent
+        dist = (
+            flat.pow(2).sum(dim=1, keepdim=True)
+            - 2 * flat @ emb.t()
+            + emb.pow(2).sum(dim=1)
+        ) # (BHW, K)
+        indices = torch.argmin(dist, dim=1) # (BHW, )
+        z_q = self.embedding(indices).view(B, H, W, D).permute(0, 3, 1, 2)
 
-    # Obtaining the loss
-    loss_code = F.mse_loss(z_q, z_e.detach())
-    loss_commit = F.mse_loss(z_e, z_q.detach())
-    vq_loss = loss_code + self.beta * loss_commit
+        # Obtaining the loss
+        loss_code = F.mse_loss(z_q, z_e.detach())
+        loss_commit = F.mse_loss(z_e, z_q.detach())
+        vq_loss = loss_code + self.beta * loss_commit
 
-    # Estimator 
-    z_q = z_e + (z_q - z_e).detach()
+        # Estimator 
+        z_q = z_e + (z_q - z_e).detach()
 
-    onehot = F.one_hot(indices, self.num_embeddings).float()
-    avg_probs = onehot.mean(dim=0)
-    perplexity = torch.exp(-(avg_probs * (avg_probs + 1e-10).log()).sum())
-    return z_q, vq_loss, perplexity, indices.view(B, H, W)
+        onehot = F.one_hot(indices, self.num_embeddings).float()
+        avg_probs = onehot.mean(dim=0)
+        perplexity = torch.exp(-(avg_probs * (avg_probs + 1e-10).log()).sum())
+        return z_q, vq_loss, perplexity, indices.view(B, H, W)
  
 class Encoder(nn.Module):
     def __init__(self, in_ch: int = 1, base: int = 64, z_dim: int = 128):
