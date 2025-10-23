280398ac37901dc3674ac7fddb58575c71ff2bfa
shruthisubramaniam <shruthisubramaniam06@gmail.com>
2025-10-23 11:35:19 +1000

    Removing helper function for scaling images as no longer needed

--- recognition/s4858817_VQVAE/train.py
+++ recognition/s4858817_VQVAE/train.py
@@ -10,10 +10,3 @@ from torchmetrics.image import StructuralSimilarityIndexMeasure
 from dataset import get_dataloader
 from modules import VQVAE
 
-# defining a function to do per image mini and max scale to [0, 1] for SSIM
-def norm_minmax(x: torch.Tensor, eps: float = 1e-6):
-    x_min = x.amin(dim=(-2, -1), keepdim=True)
-    x_max = x.amax(dim=(-2, -1), keepdim=True)
-    return (x - x_min) / (x_max - x_min + eps)
-
-# 
