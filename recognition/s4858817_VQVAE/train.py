b7b9c3d61ca0580ff49b0fddca0ce607363785aa
shruthisubramaniam <shruthisubramaniam06@gmail.com>
2025-10-23 11:42:48 +1000

    creating a function to obtain the plots. First plot created is the total loss plot

--- recognition/s4858817_VQVAE/train.py
+++ recognition/s4858817_VQVAE/train.py
@@ -10,3 +10,21 @@ from torchmetrics.image import StructuralSimilarityIndexMeasure
 from dataset import get_dataloader
 from modules import VQVAE
 
+# Creating a function that will obtain the plots and saves the training and 
+# validation metrics. 
+
+def plot_metrics(save_dir: Path, train_losses, val_losses, val_ssims, val_perplexities):
+    epochs = range(1, len(train_losses) + 1)
+
+    # Plotting the total loss 
+    plt.figure(figsize = (10,5))
+    plt.plot(epochs, train_losses, label='Total loss for training')
+    plt.plot(epochs, val_losses, label='Total loss for validation')
+    plt.title('Loss for training and validation')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(save_dir / "lost_plot.png")
+
+    
