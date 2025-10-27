4b67858dd15c08ffbb3b3ecd1e94571292bc2ced
shruthisubramaniam <shruthisubramaniam06@gmail.com>
2025-10-27 10:59:11 +1000

    Adding a small reconstruction helper that will grab one batch from the dataloader

--- recognition/s4858817_VQVAE/predict.py
+++ recognition/s4858817_VQVAE/predict.py
@@ -7,3 +7,20 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 from torchmetrics.image import StructuralSimilarityIndexMeasure
+
+from modules import VQVAE
+from dataset import SliceDataset 
+
+def reconstruction_function(model, dataloader, device, output_dir, num_images = 8):
+    print("Running Reconstruction Demonstration")
+    model.eval()
+    # Obtaining from the test set one batch of data 
+    images, paths = next(iter(dataloader))
+    images = images[:num_images].to(device)
+    with torch.no_grad():
+        reconstruction, _, _ = model(images)
+
+    
+
+
+
